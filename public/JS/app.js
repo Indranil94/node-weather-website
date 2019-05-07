eed18e328f28ea85ac560fd2a1211cae50329039
Indranil94 <indra.santra@gmail.com>
2019-05-08 01:05:58 +0530

    Enhancing Output

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,17 +1,20 @@
 const searched = document.getElementById('searchbar');
 const button = document.querySelector('button');
 const result = document.querySelector('#result');
+const location_text = document.querySelector('#location');
 
 button.addEventListener('click',(e)=>{
     e.preventDefault();
     const url = "/weather?address="+searched.value;
+    location_text.textContent = "Loading..."
     fetch(url)
     .then((res)=>res.json())
     .then(data=>{
-        console.log(data);
+
         if(data.temperature){
+            location_text.textContent = "Location: "+data.location;
             result.textContent = "The current temperature is "+data.temperature+" 'F. The probability of precipitation is "+data.precipProbability+".";
-            result.style.fontSize = "2em";
+            result.style.fontSize = "1.5em";
             result.style.color = "Blue";
             
         }
